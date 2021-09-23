d8854d82f8a899bbe5bc2a8d7375dc3ffcafff9a
Brandon <etrudcollege@gmail.com>
2021-09-23 12:41:48 -0500

    added login vue screens

---
+++ client/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <form action="" method="post">
+				<label for="username">
+					<i class="fas fa-user"></i>
+				</label>
+				<input type="text" name="username" placeholder="Username" id="username" required>
+				<label for="password">
+					<i class="fas fa-lock"></i>
+				</label>
+				<input type="password" name="password" placeholder="Password" id="password" required>
+				<input type="submit" value="Login">
+			</form>
+            <button type="button" class="btn btn-secondary">Forgot Password?</button>
+			<router-link to="/signup">Create an Account</router-link>
+  </div>
+</template>
---
+++ client/src/views/Signup.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    <h1>Create an Account</h1>
+    <form action="" method="post">
+				<label for="username">
+					<i class="fas fa-user"></i>
+				</label>
+				<input type="text" name="username" placeholder="Username" id="username" required>
+				<label for="password">
+					<i class="fas fa-lock"></i>
+				</label>
+				<input type="password" name="password" placeholder="Password" id="password" required>
+				<input type="submit" value="Create Account">
+			</form>
+  </div>
+</template>
